bd42075a11dd0ae24fa5faa70913b3cc8a4ec95e
Emmanuel <femmor2u@gmail.com>
2019-10-09 14:15:37 -0500

    Added the hero component with default properties for the homepage and error page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 const Home = () => {
-  return <div>Hello from Homepage</div>;
+  return <Hero className="defaultHero" />;
 };
 
 export default Home;
