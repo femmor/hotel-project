8606b4f152ac54926836fb4d54642533334276b8
Emmanuel <femmor2u@gmail.com>
2019-10-09 12:58:21 -0500

    Implemented the React BrowserRouter and set up the different routes, created the Navbar component and the pages components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return <div>Hello from Navbar</div>;
+  }
+}
+
+export default Navbar;
---
+++ src/pages/SingleRoom.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class SingleRoom extends Component {
+  render() {
+    return <div>Hello from Single Room Page</div>;
+  }
+}
+
+export default SingleRoom;
